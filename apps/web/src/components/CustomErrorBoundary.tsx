fd9cd7d8f5a1e239f85913f8dc97a03c8cc1f645
njzy <njzydark@gmail.com>
2022-12-25 12:27:16 +0800

    refactor(web): remove sentry

--- apps/web/src/components/CustomErrorBoundary.tsx
+++ apps/web/src/components/CustomErrorBoundary.tsx
@@ -1,6 +1,7 @@
 import { Button, Result } from '@arco-design/web-react';
-import { ErrorBoundary } from '@sentry/react';
-import { ReactNode } from 'react';
+// import * as Sentry from '@sentry/react';
+// import { BrowserTracing } from '@sentry/tracing';
+import { cloneElement, Component, ReactElement, ReactNode } from 'react';
 
 type Props = {
   children: ReactNode;
@@ -9,7 +10,44 @@ type Props = {
   subTitle?: string;
 };
 
+// Sentry.init({
+//   dsn: 'https://b330b8f226c444769a43292c20048d3e@o477756.ingest.sentry.io/6749540',
+//   integrations: [new BrowserTracing()],
+//   tracesSampleRate: 1.0
+// });
+
+type ErrorBoundaryProps = Props & { fallback: ReactNode };
+
+class ErrorBoundary extends Component<
+  ErrorBoundaryProps,
+  { error: Error | null; errorInfo: { componentStack: string } | null }
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      error: error,
+      errorInfo: errorInfo
+    });
+    console.error(error);
+  }
+
+  render() {
+    if (this.state.error) {
+      return cloneElement(this.props.fallback as ReactElement, { subTitle: this.state.error?.message });
+    }
+    return this.props.children;
+  }
+}
+
 export const CustomErrorBoundary = ({ children, title, subTitle, showDialog }: Props) => {
+  if (import.meta.env.DEV) {
+    return <>{children}</>;
+  }
+
   return (
     <ErrorBoundary
       showDialog={showDialog}
--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -1,7 +1,5 @@
 import './index.less';
 
-import * as Sentry from '@sentry/react';
-import { BrowserTracing } from '@sentry/tracing';
 import { render } from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 
@@ -9,12 +7,6 @@ const Router = HashRouter;
 
 import { App } from './app';
 import { CustomErrorBoundary } from './components/CustomErrorBoundary';
-
-Sentry.init({
-  dsn: 'https://b330b8f226c444769a43292c20048d3e@o477756.ingest.sentry.io/6749540',
-  integrations: [new BrowserTracing()],
-  tracesSampleRate: 1.0
-});
 
 const rootEl = document.createElement('div');
 rootEl.id = 'root';
